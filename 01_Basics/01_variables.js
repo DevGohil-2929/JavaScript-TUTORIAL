709fd51658d3971526541f553a314b3592f1b696
DevGohil-2929 <devgohil2753@gmail.com>
2025-08-06 11:20:36 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14453;
+let accountEmail = "example@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+//accountId = 15 (here it can't be applicable because of const)
+ console.log(accountId);
+
+ accountEmail = "hc@hc.com"
+ accountPassword = "5555"
+ accountCity = "surat"
+
+ console.table([accountEmail, accountId, accountPassword, accountCity, accountState]);
