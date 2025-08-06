4107a6869c578c4979d7f4d97ab0fc8ae676c2e7
DevGohil-2929 <devgohil2753@gmail.com>
2025-08-06 13:30:28 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionoperations.js
@@ -0,0 +1,28 @@
+let score = 33;
+let secondScore ="33ab"
+
+//console.log(typeof score);
+//console.log(typeof(secondScore));
+
+let conversion = Number(secondScore);
+
+//console.log(typeof conversion);
+//console.log(conversion);
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = true
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+ console.log(typeof stringNumber);
+
+
+
